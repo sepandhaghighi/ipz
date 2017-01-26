03b47947bd61fbdd506218c4ac98bfb5a35a2b1c
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-01-26 19:44:07 +0330

    fix : local_ip for unix version fixed

--- ipz/ipz.py
+++ ipz/ipz.py
@@ -1,16 +1,39 @@
 import socket
 import requests
 import re
+import platform
+import subprocess as sub
 ip_pattern=r"(?:[0-9]{1,3}\.){3}[0-9]{1,3}"
-api_1="http://www.ip2location.com"
+api_1="http://ipinfo.io/ip"
 
 def local_ip():
+    '''
+    return local ip of computer in windows by socket module and in unix with hostname command in shell
+    :return: local ip as string
+    '''
     try:
-        return socket.gethostbyname(socket.gethostname())
-    except:
+        ip=socket.gethostbyname(socket.gethostname())
+        if ip!="127.0.0.1":
+            return ip
+        elif platform.system()!="Windows":
+            command=sub.Popen(["hostname","-I"],stdout=sub.PIPE,stderr=sub.PIPE,stdin=sub.PIPE,shell=False)
+            response=list(command.communicate())
+            if len(response[0])>0:
+                return str(response[0])[2:-4]
+            else:
+                return "Error"
+        else:
+            return "Error"
+
+    except Exception as e:
+        print(e)
         return "Error"
 
 def global_ip():
+    '''
+    retur ip with by http://ipinfo.io/ip api
+    :return: global ip as string
+    '''
     try:
         new_session=requests.session()
         response=new_session.get(api_1)
@@ -19,11 +42,4 @@ def global_ip():
         return ip_list[0]
     except:
         return "Error"
-
-
-
-
-
-if __name__=="__main__":
-    print(global_ip())
 
