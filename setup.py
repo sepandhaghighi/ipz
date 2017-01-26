e39a571069a5d4d079657e61fec12b0e95d85860
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-01-26 18:46:32 +0330

    setup file added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'ipz',
+  packages = ['ipz'],
+  version = '0.1',
+  description = 'Global ip finder library for python',
+  long_description="",
+  author = 'Sepand Haghighi',
+  author_email = 'sepand.haghighi@yahoo.com',
+  url = 'https://github.com/sepandhaghighi/ipz',
+  download_url = 'https://github.com/sepandhaghighi/ipz/tarball/v0.1',
+  keywords = ['free', 'ip', 'library','python','ipz','globalip','localip'],
+  classifiers = [],
+  license='MIT',
+)
